fc0f96f0ef85a1360708dd30e9ea91cfab3ddee6
ZanderKoch <89077492+ZanderKoch@users.noreply.github.com>
2021-11-12 13:55:45 +0100

    started main function

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 /**
  * @file script for a webapp that lets users book rooms and calculates prices
  * @author Zander Koch
- * @version 1
+ * @version 1.0
  */
+
+
+/////////////
+//functions//
+/////////////
+
+/**
+ * main entry point for the script
+ */
+function main(){
+
+    //html reference initilization
+    rbtnRoomType = document.querySelectorAll("input[type=radio]")
+    slctRoomGuests = document.querySelector("")
+}
