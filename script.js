9bbf53de321ac69a16350fe3151faef01d7567c3
ZanderKoch <89077492+ZanderKoch@users.noreply.github.com>
2021-11-16 11:25:09 +0100

    updated the way the form is handled, updated code to follow

--- script.js
+++ script.js
@@ -10,6 +10,8 @@
 /**@type {Number} */
 let dailyRoomCost;
 
+/**@type {Element} */
+let formElem
 
 /////////////
 //functions//
@@ -24,62 +26,37 @@ let dailyRoomCost;
 //I'm aware this doesn't follow SRP but i don't think it's worth writing two 
 //functions in such a small project
 function checkRoomSelect(){
-    if(rbtnRoomType[0].checked){
-        slctRoomGuests.disabled = true;
-        chkExtraLakeView.disabled = false;
-        dailyRoomCost = 600;
+    if(formElem.roomType[0].checked){
+        formElem.persons.disabled = true;
+        formElem.addition[2].disabled = false;
+        dailyRoomCost = formElem.roomType.value.split(",")[1];
     }
-    else if(rbtnRoomType[1].checked){
-        slctRoomGuests.disabled = true;
-        chkExtraLakeView.disabled = false;
-        dailyRoomCost = 800;
+    else if(formElem.roomType[1].checked){
+        formElem.persons.disabled = true;
+        formElem.addition[2].disabled = false;
+        dailyRoomCost = formElem.roomType.value.split(",")[1];
     }
-    else if(rbtnRoomType[2].checked){
-        slctRoomGuests.disabled = false;
-        chkExtraLakeView.disabled = true;
-        dailyRoomCost = 950
+    else if(formElem.roomType[2].checked){
+        formElem.persons.disabled = false;
+        formElem.addition[2].disabled = true;
+        formElem.addition[2].checked = false;
+        dailyRoomCost = parseInt(formElem.roomType.value.split(",")[1]);
     }
     console.log("dailyRoomCost: " + dailyRoomCost) //debug
 }
-
-
 
 /**
  * main entry point for the script
  */
 function main(){
-
-    //html reference initilization
-    let rbtnRoomTypes = document.querySelectorAll("input[type=radio]");
-    console.log(rbtnRoomTypes); //debug
-    let slctRoomGuests = document.querySelector("#room > * > select");
-    console.log(slctRoomGuests); //debug
-    
-    let slctTimeStaying = document.querySelector("#date > * > select");
-    console.log(slctTimeStaying); //debug
-
-    let chkExtraInternet = document.querySelectorAll("input[type=checkbox]")[0];
-    console.log(chkExtraInternet); //debug
-    let chkExtraParking = document.querySelectorAll("input[type=checkbox]")[1];
-    console.log(chkExtraParking); //debug
-    let chkExtraLakeView = document.querySelectorAll("input[type=checkbox]")[2];
-    console.log(chkExtraLakeView); //debug
-
-    let spnTotalCost = document.querySelector("totalCost");
-    console.log(spnTotalCost); //debug
-
-    let txtCampaignCode = document.querySelector("input[name=campaigncode]");
-    console.log(txtCampaignCode); //debug
-
-    let txtCustomerZipcode = document.querySelector("input[name=zipcode]");
-    console.log(txtCustomerZipcode); //debug
-    let txtCustomerCity = document.querySelector("input[name=city]");
-    console.log(txtCustomerCity); //debug
-    let txtCustomerPhoneNumber = document.querySelector("input[name=telephone]")
-    console.log(txtCustomerPhoneNumber); //debug
+    //initializing reference to form
+    formElem = document.querySelector("#booking");
+    //initialising reference to 
+    let spnTotalCost = document.querySelector("#totalCost");
 
     //adding event listeners
-    for(element in rbtnRoomTypes){
+    for(element of formElem.roomType){
+        console.log(element) //debug
         addListener(element, "click", checkRoomSelect);
     }
 }
